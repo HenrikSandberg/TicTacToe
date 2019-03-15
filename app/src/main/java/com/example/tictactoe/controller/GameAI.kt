d74d217fd50b4b5000dcf9409378c2bae9672f1c
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-03-15 13:17:38 +0100

    Update GameAI.kt

--- app/src/main/java/com/example/tictactoe/controller/GameAI.kt
+++ app/src/main/java/com/example/tictactoe/controller/GameAI.kt
@@ -1,9 +1,8 @@
 package com.example.tictactoe.controller
 
-
 class GameAI {
     fun makeMove(board: Array<Int>): Int {
-        (0..1).forEach { //first looking for winning options, then looking to defend
+        (0..1).forEach { //first looking for winning options, then looking to defend. This is represented with 1 and -1
             val opportunity =  attackOrDefend(board, if (it == 0) 1 else -1)
             if (opportunity != -1) return opportunity
         }
@@ -22,9 +21,9 @@ class GameAI {
 
     private fun attackOrDefend(board: Array<Int>, player: Int): Int {
         val possibleMoves = listOf(
-            rowOpportunities(board.clone(), player),
-            columnOpportunities(board.clone(), player),
-            crossOpportunities(board.clone(), player)
+            rowOpportunities(board, player),
+            columnOpportunities(board, player),
+            crossOpportunities(board, player)
         ).shuffled() // Make sure the code don't favor column, row or cross over one and other
 
         (0 until possibleMoves.size).forEach { item ->
@@ -32,55 +31,55 @@ class GameAI {
         }
         return -1
     }
-}
 
-private fun rowOpportunities(board: Array<Int>, player: Int): Int {
-    var row = 0
-    
-    do {
-        if ((board[row] + board[row+1] + board[row+2]) == 2 * player){
-            (0..2).forEach { position ->
-                if (board[row+position] == 0) return row+position
+    private fun rowOpportunities(board: Array<Int>, player: Int): Int {
+        var row = 0
+
+        do {
+            if ((board[row] + board[row+1] + board[row+2]) == 2 * player){
+                (0..2).forEach { position ->
+                    if (board[row+position] == 0) return row+position
+                }
+            }
+            row += 3
+        } while (row < board.size)
+
+        return -1
+    }
+
+    private fun columnOpportunities(board: Array<Int>, player: Int): Int {
+        (0..2).forEach { column ->
+
+            if ( (board[column] + board[column+3] + board[column+6]) == (2 * player)) {
+
+                var item = 0
+                do {
+                    if(board[column+item] == 0) return column+item
+                    item += 3
+                } while (item < 9)
             }
         }
-        row += 3
-    } while (row < board.size)
-    
-    return -1
-}
+        return -1
+    }
 
-private fun columnOpportunities(board: Array<Int>, player: Int): Int {
-    (0..2).forEach { column ->
+    private fun crossOpportunities(board: Array<Int>, player: Int): Int {
+        var position = 0
 
-        if ( (board[column] + board[column+3] + board[column+6]) == (2 * player)) {
+        if ((board[0] + board[4] + board[8]) == (2 * player)) {
 
-            var item = 0
             do {
-                if(board[column+item] == 0) return column+item
-                item += 3
-            } while (item < 9)
+                if (board[position] == 0) return position
+                position += 4
+            } while (position < 9)
+
+        } else if ((board[2] + board[4] + board[6]) == (2 * player)) {
+
+            position = 2
+            do {
+                if (board[position] == 0) return position
+                position += 2
+            } while (position < 9)
         }
+        return -1
     }
-    return -1
-}
-
-private fun crossOpportunities(board: Array<Int>, player: Int): Int {
-    var position = 0
-
-    if ((board[0] + board[4] + board[8]) == (2 * player)) {
-
-        do {
-            if (board[position] == 0) return position
-            position += 4
-        } while (position < 9)
-
-    } else if ((board[2] + board[4] + board[6]) == (2 * player)) {
-
-        position = 2
-        do {
-            if (board[position] == 0) return position
-            position += 2
-        } while (position < 9)
-    }
-    return -1
 }
