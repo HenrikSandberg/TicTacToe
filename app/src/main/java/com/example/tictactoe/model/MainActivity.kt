3a71c6e7e2d8fb4f8bd1dd0a2e7cefd8b9564d92
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-03-25 11:54:23 +0100

    Made fragmentManager a function call

--- app/src/main/java/com/example/tictactoe/model/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/model/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.FragmentTransaction.*
 import com.example.tictactoe.R
 import com.example.tictactoe.controller.GameMode
 import com.example.tictactoe.controller.TicTakToe
-import kotlinx.android.synthetic.main.fragment_choose_player.*
 
 class MainActivity : AppCompatActivity() {
     private val fragmentManager = supportFragmentManager
@@ -33,28 +32,14 @@ class MainActivity : AppCompatActivity() {
     fun setUpPVPGame(){
         val choosePlayerOne = ChoosePlayerFragment()
         choosePlayerOne.setInstance(1)
-
-        fragmentManager
-            .beginTransaction()
-            .setTransition(TRANSIT_FRAGMENT_FADE)
-            //.setCustomAnimations(R.animator.slide_out_right, R.animator.slide_in_left)
-            .replace(R.id.game_content_frame, choosePlayerOne)
-            .addToBackStack(null)
-            .commit()
+        addToFragmentManagerWithBackStack(choosePlayerOne)
     }
 
     fun setUpPlayerTwo(PlayerOneName: String){
         playerOne = PlayerOneName
         val choosePlayerTwo = ChoosePlayerFragment()
         choosePlayerTwo.setInstance(2)
-
-        fragmentManager
-            .beginTransaction()
-            .setTransition(TRANSIT_FRAGMENT_FADE)
-            //.setCustomAnimations(R.animator.slide_out_right, R.animator.slide_in_left)
-            .replace(R.id.game_content_frame, choosePlayerTwo)
-            .addToBackStack(null)
-            .commit()
+        addToFragmentManagerWithBackStack(choosePlayerTwo)
     }
 
     fun goToPVPGame(playerTwoName: String){
@@ -66,36 +51,25 @@ class MainActivity : AppCompatActivity() {
     fun setUpAIGame(){
         val choosePlayer = ChoosePlayerFragment()
         choosePlayer.setInstance(3)
-
-        fragmentManager
-            .beginTransaction()
-            .setTransition(TRANSIT_FRAGMENT_FADE)
-            //.setCustomAnimations(R.animator.slide_out_right, R.animator.slide_in_left)
-            .replace(R.id.game_content_frame, choosePlayer)
-            .addToBackStack(null)
-            .commit()
+        addToFragmentManagerWithBackStack(choosePlayer)
     }
 
     fun setUpAI(playerName: String){
         playerOne = playerName
-        fragmentManager
-            .beginTransaction()
-            .setTransition(TRANSIT_FRAGMENT_FADE)
-            //.setCustomAnimations(R.animator.slide_out_right, R.animator.slide_in_left)
-            .replace(R.id.game_content_frame, SetUpGameAgainstAIFragment())
-            .addToBackStack(null)
-            .commit()
+        addToFragmentManagerWithBackStack( SetUpGameAgainstAIFragment())
     }
 
     fun getPlayerOne(): String = playerOne ?: "Not Selected"
 
-    fun setUpHighScore(){
-        println("Set Up high score!")
+    fun setUpHighScore(){ addToFragmentManagerWithBackStack(HighScoreFragment()) }
+
+    private fun addToFragmentManagerWithBackStack(fragment: Fragment) {
         fragmentManager
             .beginTransaction()
             .setTransition(TRANSIT_FRAGMENT_FADE)
             //.setCustomAnimations(R.animator.slide_out_right, R.animator.slide_in_left)
-            .replace(R.id.game_content_frame, HighScoreFragment())
+            .replace(R.id.game_content_frame, fragment)
+            .addToBackStack(null)
             .commit()
     }
 
@@ -108,17 +82,5 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    private fun setUpGame(game: TicTakToe) {
-        fragmentManager
-            .beginTransaction()
-            .setTransition(TRANSIT_FRAGMENT_FADE)
-            //.setTransition(TRANSIT_FRAGMENT_OPEN)
-            //.setCustomAnimations(R.animator.slide_out_right, R.animator.slide_in_left)
-            .replace(R.id.game_content_frame, BoardFragment(game))
-            .addToBackStack(null)
-            .commit()
-    }
-
-    //TODO: Create winn fragemnt this fragment should give you two opperuneties, go back to main menu, or play again
-    //TODO: Update database when game is over
+    private fun setUpGame(game: TicTakToe) {  addToFragmentManagerWithBackStack(BoardFragment(game)) }
 }
