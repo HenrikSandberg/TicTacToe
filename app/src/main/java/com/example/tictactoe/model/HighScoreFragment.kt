f523f5e0dd1f5b2985766e914aa5fce3d751ec9f
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-03-25 20:36:18 +0100

    Still no working RecyclerView

--- app/src/main/java/com/example/tictactoe/model/HighScoreFragment.kt
+++ app/src/main/java/com/example/tictactoe/model/HighScoreFragment.kt
@@ -1,16 +1,104 @@
 package com.example.tictactoe.model
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tictactoe.R
+
+import com.example.tictactoe.model.dummy.DummyContent
+import com.example.tictactoe.model.dummy.DummyContent.DummyItem
+import kotlinx.android.synthetic.main.fragment_person_list.*
+
+/**
+ * A fragment representing a list of Items.
+ * Activities containing this fragment MUST implement the
+ * [HighScoreFragment.OnListFragmentInteractionListener] interface.
+ */
 class HighScoreFragment : Fragment() {
+
+    // TODO: Customize parameters
+    private var columnCount = 1
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_high_score, container, false)
+        val view = inflater.inflate(R.layout.fragment_person_list, container, false)
+        // Set the adapter
+
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(activity)
+                    else -> GridLayoutManager(activity, columnCount)
+                }
+                adapter = MypersonRecyclerViewAdapter(DummyContent.ITEMS, activity as MainActivity)
+            }
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson
+     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: DummyItem?)
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            HighScoreFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/tictactoe/model/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/model/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.tictactoe.model
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tictactoe.R
 import com.example.tictactoe.controller.GameMode
 import com.example.tictactoe.controller.TicTakToe
+import com.example.tictactoe.model.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HighScoreFragment.OnListFragmentInteractionListener {
     private val fragmentManager = supportFragmentManager
     private lateinit var game:TicTakToe
     private var playerOne: String? = null
     private var playerTwo: String? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,47 +25,54 @@ class MainActivity : AppCompatActivity() {
         setUpMainMenu()
     }
 
-    fun createGame(gameMode: GameMode){
-        game = when (gameMode == GameMode.PVP){
+    fun createGame(gameMode: GameMode) {
+        game = when(gameMode == GameMode.PVP) {
             true -> TicTakToe(playerOne!!, playerTwo!!)
-            else -> TicTakToe(playerOne!!, gameMode)
+            false -> TicTakToe(playerOne!!, gameMode)
         }
         setUpGame(game)
     }
 
-    fun setUpPVPGame(){
+    fun setUpPVPGame() {
         val choosePlayerOne = ChoosePlayerFragment()
         choosePlayerOne.setInstance(1)
         addToFragmentManagerWithBackStack(choosePlayerOne)
     }
 
-    fun setUpPlayerTwo(PlayerOneName: String){
+    fun setUpPlayerTwo(PlayerOneName: String) {
         playerOne = PlayerOneName
         val choosePlayerTwo = ChoosePlayerFragment()
         choosePlayerTwo.setInstance(2)
         addToFragmentManagerWithBackStack(choosePlayerTwo)
     }
 
-    fun goToPVPGame(playerTwoName: String){
+    fun goToPVPGame(playerTwoName: String) {
         playerTwo = playerTwoName
         game = TicTakToe(playerOne!!, playerTwo!!)
         setUpGame(game)
     }
 
-    fun setUpAIGame(){
+    fun setUpAIGame() {
         val choosePlayer = ChoosePlayerFragment()
         choosePlayer.setInstance(3)
         addToFragmentManagerWithBackStack(choosePlayer)
     }
 
-    fun setUpAI(playerName: String){
+    fun setUpAI(playerName: String) {
         playerOne = playerName
         addToFragmentManagerWithBackStack( SetUpGameAgainstAIFragment())
     }
 
     fun getPlayerOne(): String = playerOne ?: "Not Selected"
 
-    fun setUpHighScore(){ addToFragmentManagerWithBackStack(HighScoreFragment()) }
+    fun setUpHighScore(){
+        println("Show High score!")
+        addToFragmentManagerWithBackStack(HighScoreFragment())
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?){
+        println("Something happens")
+    }
 
     private fun addToFragmentManagerWithBackStack(fragment: Fragment) {
         fragmentManager
